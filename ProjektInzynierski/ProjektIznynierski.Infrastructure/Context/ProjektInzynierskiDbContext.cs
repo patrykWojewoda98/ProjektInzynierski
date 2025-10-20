3828da0af22c4f83db7e3e45e8ffa050709cf49f
Patryk Wojewoda <p.wojewoda1998@gmail.com>
2025-10-20 17:16:04 +0200

    Add Entity Framework Core, configure database connection, and register Infrastructure services in API Project

---
+++ ProjektInzynierski/ProjektIznynierski.Infrastructure/Context/ProjektInzynierskiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjektIznynierski.Domain.Entities;
+
+namespace ProjektIznynierski.Infrastructure.Context
+{
+    internal class ProjektInzynierskiDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public ProjektInzynierskiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ProjektInzynierski");
+        }
+
+    }
+}
---
+++ ProjektInzynierski/ProjektIznynierski.Infrastructure/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProjektIznynierski.Infrastructure.Context;
+
+namespace ProjektIznynierski.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ProjektInzynierskiDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("ProjektInzynierskiDatabase");
+                options.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
