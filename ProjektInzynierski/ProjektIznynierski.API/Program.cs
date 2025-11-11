02ba97cc1bee3a58de2826446a5aff2754edaeb9
Patryk Wojewoda <p.wojewoda1998@gmail.com>
2025-11-11 10:22:33 +0100

    Added to the Presentation layer an Extensions class to configure API presentation services. Registered Swagger, controllers, and HTTPS redirection through AddPresentation() and UsePresentation() extension methods. Integrated the Presentation layer in Program.cs and added necessary NuGet packages: Microsoft.AspNetCore.Hosting, Microsoft.Extensions.DependencyInjection, Swashbuckle.AspNetCore.Annotations, Swashbuckle.AspNetCore.Swagger, Swashbuckle.AspNetCore.SwaggerGen, and Swashbuckle.AspNetCore.SwaggerUI.

--- ProjektInzynierski/ProjektIznynierski.API/Program.cs
+++ ProjektInzynierski/ProjektIznynierski.API/Program.cs
@@ -1,15 +1,19 @@
 using ProjektIznynierski.Infrastructure;
 using ProjektIznynierski.Application;
+using ProjektIznynierski.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+builder.Services.AddPresentation();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UsePresentation();
 
 
 app.Run();
